73a6bbc4a473800f0ff62eda8554d993b90a4c3b
Vahid Vakili <v.vakili73@gmail.com>
2018-12-15 18:20:53 +0330

    Commit...

--- Utils/sklearn/metrics/pairwise.py
+++ Utils/sklearn/metrics/pairwise.py
@@ -1111,7 +1111,7 @@ _VALID_METRICS = ['euclidean', 'l2', 'l1', 'manhattan', 'cityblock',
                   'braycurtis', 'canberra', 'chebyshev', 'correlation',
                   'cosine', 'dice', 'hamming', 'jaccard', 'kulsinski',
                   'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto',
-                  'russellrao', 'seuclidean', 'sokalmichener', 'kullbackleibler'
+                  'russellrao', 'seuclidean', 'sokalmichener', 'kullbackleibler',
                   'sokalsneath', 'sqeuclidean', 'yule', "wminkowski"]
 
 
